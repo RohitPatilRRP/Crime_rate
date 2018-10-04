0f892e7046bddbd20807b691e36ca3d2843c9c97
Rohit Patil <32801968+RohitPatilRRP@users.noreply.github.com>
2018-10-05 00:02:02 +0530

    rohit

--- venv/app/app.py
+++ venv/app/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__,static_url_path='/static')
 app.debug = True
 
-
 @app.route('/')
 def index():
+    return render_template('/index.html')
+
+@app.route('/index')
+def dashboard():
     return render_template('/index.html')
 
 if __name__ == "__main__":
